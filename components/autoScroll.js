e3fef7817b12faef8fc523e9b06f835e51f4c9a3
Nico <nicogu.safe@gmail.com>
2023-01-31 15:09:58 -0300

    Minor changes, reindented

--- components/autoScroll.js
+++ components/autoScroll.js
@@ -1,19 +1,18 @@
 // https://github.com/chenxiaochun/blog/issues/38
 module.exports = async page => {
-  await page.evaluate(async () => {
-    await new Promise((resolve) => {
-      let totalHeight = 0
-      let distance = 500
-      let timer = setInterval(() => {
-        let scrollHeight = document.body.scrollHeight
-        window.scrollBy(0, distance)
-        totalHeight += distance
-
-        if(totalHeight >= scrollHeight - window.innerHeight) {
-          clearInterval(timer)
-          resolve()
-        }
-      }, 20)
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            let totalHeight = 0
+            let distance = 100
+            let timer = setInterval(() => {
+                scrollBy(0, distance)
+                totalHeight += distance
+                
+                if (totalHeight >= innerHeight * 1.6) {
+                    clearInterval(timer)
+                    resolve()
+                }
+            }, 10)
+        })
     })
-  })
 }
